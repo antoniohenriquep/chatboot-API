f6334db1439feacfc7e5b7e033475949eec22114
antoniohenriquep <antoniohenriquep@outlook.com>
2024-08-05 18:17:23 -0300

    feat: Model de usuario, rotas

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+
+const router = Router()
+
+router.post('/register', (req,res)=>{
+
+})
+
+router.post('/session', (req,res)=>{
+
+})
+
+module.exports = {router}
